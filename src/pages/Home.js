87afabaf5175498d7be1369ac453e70e916a3bfd
Jonathan Benn <bonjennprojects@gmail.com>
2024-02-07 14:46:33 -0800

    Additional styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
-import Nav from '../components/Nav';
+import Nav from '../components/Nav'
+import AuthModal from "../components/AuthModal"
+import { useState } from 'react'
+
 
 
 const Home = () => {
+
+    const [ showModal, setShowModal ] = useState(false)
 
     const authToken = false
 
     const handleClick = ()=> {
         console.log('clicked')
+        setShowModal(true)
     }
 
 
@@ -18,6 +24,10 @@ const Home = () => {
                 <button className ="primary-button" onClick={handleClick}>
                     {authToken ? 'Signout' : 'Create Account'}
                 </button>
+
+                {showModal && (
+                    <AuthModal setShowModal={setShowModal}/>
+                )}
             </div>
         </div>
     )
