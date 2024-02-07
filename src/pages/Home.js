d8fa07c23861fe636ce02c65e33e05a95beea7fa
Jonathan Benn <bonjennprojects@gmail.com>
2024-02-07 11:52:01 -0800

    Styled fonts and sign out button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
+import Nav from '../components/Nav';
+
+
 const Home = () => {
+
+    const authToken = true
+
+    const handleClick = ()=> {
+        console.log('clicked')
+    }
+
+
     return (
-        <div>
-            Home
+        <>
+        <Nav/>
+        <div className="home">
+            <h1>The World's Most Sought-After Singles</h1>
+            <button className ="primary-button" onClick={handleClick}>
+                {authToken ? 'Signout' : 'Create Account'}
+            </button>
         </div>
+        </>
     )
 }
 export default Home
