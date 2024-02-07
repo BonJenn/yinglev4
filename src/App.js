48197c53d2c6cdea58c3b3189deb868862a87f79
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2024-02-07 09:43:28 -0800

    Set up pages and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import Home from './pages/Home'
+import Onboarding from './pages/Onboarding'
+import Dashboard from './pages/Dashboard'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+
 const App = () => {
   return (
-    <div>
-    
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
