ef9876a93c1b2c1d5e2a004104113d7467cbe1ad
Jonathan Benn <bonjennprojects@gmail.com>
2024-02-09 13:46:19 -0800

    Added inputs to m odal

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,12 +1,56 @@
+import { useState } from 'react'
+
 const AuthModal = ({setShowModal}) => {
+    const [ email, setEmail ] = useState(null)
+    const [ password, setPassword ] = useState(null)
+    const [ confirmPassword, setConfirmPassword ] = useState(null)
+    const [ error, setError ] = useState(null)
+
+    console.log(email, password, confirmPassword)
 
     const handleClick = () => {
         setShowModal(false)
-
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const isSignUp = true
+
     return (
         <div className="auth-modal">
-            <div onClick={handleClick}>x</div>
+            <div className="close-icon" onClick={handleClick}>x</div>
+            <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN' }</h2>
+            <p>By clicking Log In you agree to the terms and conditions</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="email"
+                    required={true}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="password"
+                    required={true}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                 <input
+                    type="confirm password"
+                    id="confirm password"
+                    name="confirm password"
+                    placeholder="confirm password"
+                    required={true}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+
+            </form>
             AUTH MODAL
         </div>
     )
