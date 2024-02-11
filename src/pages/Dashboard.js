aa6c4ed1ce585d559e8d277a302a73ea4ee9d81c
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2024-02-10 22:34:03 -0800

    Added swiping feature

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,61 @@
+import React from 'react';
+import TinderCard from 'react-tinder-card';
+import { useState } from 'react';
+
 const Dashboard = () => {
-    return (
-        <div> Dashboard </div>
-    )
-}
-export default Dashboard
+    const characters = [
+        {
+          name: 'Richard Hendricks',
+          url: 'https://upload.wikimedia.org/wikipedia/en/3/3b/SpongeBob_SquarePants_character.svg'
+        },
+        {
+          name: 'Erlich Bachman',
+          url: 'https://upload.wikimedia.org/wikipedia/en/3/3b/SpongeBob_SquarePants_character.svg'
+        },
+        {
+          name: 'Monica Hall',
+          url: 'https://upload.wikimedia.org/wikipedia/en/3/3b/SpongeBob_SquarePants_character.svg'
+        },
+        {
+          name: 'Jared Dunn',
+          url: 'https://upload.wikimedia.org/wikipedia/en/3/3b/SpongeBob_SquarePants_character.svg'
+        },
+        {
+          name: 'Dinesh Chugtai',
+          url: 'https://upload.wikimedia.org/wikipedia/en/3/3b/SpongeBob_SquarePants_character.svg'
+        }
+      ]
+      
+   
+        const [lastDirection, setLastDirection] = useState()
+      
+        const swiped = (direction, nameToDelete) => {
+          console.log('removing: ' + nameToDelete)
+          setLastDirection(direction)
+        }
+      
+        const outOfFrame = (name) => {
+          console.log(name + ' left the screen!')
+        }
+      
+        return (
+          <div>
+            <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+            <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+            <h1>React Tinder Card</h1>
+            <div className='cardContainer'>
+              {characters.map((character) =>
+                <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                  <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                    <h3>{character.name}</h3>
+                  </div>
+                </TinderCard>
+              )}
+            </div>
+            {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+          </div>
+        )
+      }
+
+
+export default Dashboard;
