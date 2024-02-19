d04fb0f0d775b75aa8b77cef4b8ec281e98e0f2e
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2024-02-18 20:58:08 -0800

    Matches now display above chat area

--- client/src/components/MatchesDisplay.js
+++ client/src/components/MatchesDisplay.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useState
     , useEffect } from 'react'
 
-const MatchesDisplay = ({ matches }) => {
+const MatchesDisplay = ({ matches, setClickedUser }) => {
     const [matchedProfiles, setMatchedProfiles] = useState(null)
     
     const matchedUserIds = matches.map(({ user_id }) => user_id)
@@ -24,12 +24,17 @@ const MatchesDisplay = ({ matches }) => {
 
 
 
-    console.log(matchedProfiles)
-    
     
     return (
         <div className="matches-display">
-
+            {matchedProfiles?.map((match) => (
+                <div key={match.id} className="match-card" onClick={() => {setClickedUser(match)}}>
+                    <div className="img-container">
+                        <img src={match?.url} alt={`${match?.first_name} profile`} />
+                    </div>
+                    <h3>{match?.first_name}</h3>
+                </div>
+            ))}
         </div>
     )
 }
